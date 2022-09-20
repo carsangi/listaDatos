e00fa983ec99d0180f4ec67d25ad1d46ac9ebc5a
carsangi <krlos96sg@gmail.com>
2022-09-20 17:30:31 -0500

    intento dateMonth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, FormControl} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ConsultaAPIService } from './services/consulta-api.service'
 import { NgChartsModule } from 'ng2-charts';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDatepickerModule, MatDatepicker} from '@angular/material/datepicker';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
-import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MAT_DATE_LOCALE,DateAdapter, MAT_DATE_FORMATS, } from '@angular/material/core';
+import { MatMomentDateModule,MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatRadioModule } from '@angular/material/radio';
 import { NavegationComponent } from './components/navegation/navegation.component';
@@ -47,6 +47,8 @@ import { ActivationrecordComponent } from './components/activationrecord/activat
     MatFormFieldModule,
     MatCheckboxModule,
     MatRadioModule,
+  
+
   ],
   providers: [
     ConsultaAPIService,
